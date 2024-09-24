73668670ff3ec7852c90624f3353d594f0509fb6
Alec Chall <65779767+TriangleWatermelon@users.noreply.github.com>
2024-09-24 12:55:27 -0500

    Added all cards

--- Assets/Scripts/Cards/Card.cs
+++ Assets/Scripts/Cards/Card.cs
@@ -5,9 +5,22 @@ public class Card : ScriptableObject
 {
     public int value;
 
+    public int numInDeck;
+
     public string cardName;
 
     public Sprite cardImage;
 
     public string description;
+
+    public bool hasEffect = false;
+
+    public enum PlayerRestriction{
+        None,
+        Caynians,
+        Druzhnny,
+        Oogloids,
+        Rhoz,
+    }
+    public PlayerRestriction playerRestriction = PlayerRestriction.None;
 }
